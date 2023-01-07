460e0c420028a98779cef9fb8ab1b0d9a2915678
unknown <tolulope.adekunte@nicholasidoko.com>
2023-01-08 00:53:13 +0100

    first

---
+++ index.php
@@ -0,0 +1,454 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Adekunte Tolulope (Adtrex)</title>
+  <link rel="stylesheet" type="text/css" href="main.css">
+  <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1, maximum-scale=1" />
+  <meta name="description" content="Adekunte Tolulope (Adtrex) is a FullStack developer and the founder of www.techunknown.com">
+
+    <!-- Bootstrap CSS -->
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.7/css/all.css">
+    <link rel="stylesheet" type="text/css" href="float.css">
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+
+  <!--  <style>
+.editor-stage .snow {
+  height:50px;
+  background: #fff;
+}
+.snow{
+  position:fixed;
+  pointer-events:none;
+  top:0;
+  left:0;
+  right:0;
+  bottom:0;
+  height:100vh;
+  background: none;
+  background-image: url('https://s3-eu-west-1.amazonaws.com/static-ressources/s1.png'), url('https://s3-eu-west-1.amazonaws.com/static-ressources/s2.png'), url('https://s3-eu-west-1.amazonaws.com/static-ressources/s3.png');
+  z-index:100;
+  -webkit-animation: snow 10s linear infinite;
+  -moz-animation: snow 10s linear infinite;
+  -ms-animation: snow 10s linear infinite;
+  animation: snow 10s linear infinite;
+}
+@keyframes snow {
+  0% {background-position: 0px 0px, 0px 0px, 0px 0px;}
+  50% {background-position: 500px 500px, 100px 200px, -100px 150px;}
+  100% {background-position: 500px 1000px, 200px 400px, -100px 300px;}
+}
+@-moz-keyframes snow {
+  0% {background-position: 0px 0px, 0px 0px, 0px 0px;}
+  50% {background-position: 500px 500px, 100px 200px, -100px 150px;}
+  100% {background-position: 400px 1000px, 200px 400px, 100px 300px;}
+}
+@-webkit-keyframes snow {
+  0% {background-position: 0px 0px, 0px 0px, 0px 0px;}
+  50% {background-position: 500px 500px, 100px 200px, -100px 150px;}
+  100% {background-position: 500px 1000px, 200px 400px, -100px 300px;}
+}
+@-ms-keyframes snow {
+  0% {background-position: 0px 0px, 0px 0px, 0px 0px;}
+  50% {background-position: 500px 500px, 100px 200px, -100px 150px;}
+  100% {background-position: 500px 1000px, 200px 400px, -100px 300px;}
+}
+</style>-->
+  
+</head>
+<body class="body">
+  <!--<div class="snow" >
+  </div>-->
+  <header>
+  <div class="bs-example">
+    <nav class="navbar navbar-expand-md ">
+      <div style="margin-left: 20px;">
+      <a class="navbar-brand" href="#">
+        <span class="icon-rotate">
+    <img src="https://res.cloudinary.com/de8awjxjn/image/upload/v1595353106/RECTANGLE.png" width="30" height="30" class="d-inline-block align-top" alt="">
+  </span>
+    <span class="text-white" style="font-family: 'Roboto', sans-serif;">Ad</span><span class="orange" style="font-family: 'Roboto', sans-serif;">Trex</span>
+  </a></div>
+        
+
+          <div class="collapse navbar-collapse">
+            <div class="navbar-nav ml-auto ">
+              <div class="nav-margin " >
+                <a href="#home" class="nav-item nav-link active text-white "><span class="orange"> HOME</span></a></div>
+                <div class="nav-margin">
+                <a href="#project" class="nav-item nav-link active text-white nav-merge ">PROJECTS</a></div>
+                <div class="nav-margin">
+                <a href="#about" class="nav-item nav-link active text-white nav-merge">
+                  <span class="orange">ABOUT ME</span>
+                </a>
+                </div>
+                <div class="nav-margin">
+                <a href="#skills" class="nav-item nav-link active text-white nav-merge">SKILLS</a></div>
+                <div class="nav-margin">
+                <a href="#contact" class="nav-item nav-link active text-white nav-merge">
+                  <span class="orange">CONTACT</span>
+                </a></div>
+            </div>
+          </div>
+          <div class="navbar-nav ml-auto ">
+            <li class="btn-sidenav"><span style="font-size:30px;cursor:pointer" onclick="openNav()"><i class="fa fa-bars" aria-hidden="true"></i> </span></li>
+          </div>
+
+          <div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a onclick="closeNav()" href="#home">HOME</a>
+  <a onclick="closeNav()" href="#project">PROJECTS</a>
+  <a onclick="closeNav()" href="#about">ABOUT ME</a>
+  <a onclick="closeNav()" href="#skills">SKILLS</a>
+  <a onclick="closeNav()" href="#contact">CONTACT</a>
+</div>
+            
+        
+    </nav>
+</div>
+<!--<nav class="nav-color nav-font navbar navbar-expand-md">
+    
+    
+        <ul class="nav-lef nav navbar-nav navbar-left">
+    <li><img src="rectangle.png" width="30" height="30" class="" alt=""></li>
+    <li>
+      <span class="text-white" style="font-family: 'Roboto', sans-serif;">Ad</span><span class="orange" style="font-family: 'Roboto', sans-serif;">Trex</span>
+    </li></ul>
+  
+
+
+
+    <ul class="nav-righ nav navbar-nav navbar-right">
+    <li>HOME</li>
+    <li>PROJECTS</li>
+    <li>ABOUT ME</li>
+    <li>SKILLS</li>    
+    <li class="btn-sidenav">
+      <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; 
+    </li>
+  </ul>
+
+</nav>-->
+
+
+</header>
+
+<div class="container" id="home"> 
+  <center>
+   <div class="row slideanim" style=" margin-top: 8%;"> 
+      <div class="col-sm text-white pic-align" style="padding-left: 0px; " ><img src="tolu.jpg" width="200" height="200" style="border-radius: 100%;  "></div>
+      <div class="col-sm text-white about-align" style="margin-top: 40.1px;  ">
+
+        <p style="margin-bottom: 8px;">HELLO!</p>
+        <p style="margin-bottom: 8px;">I'M AD<span class="orange">TREX</span></p>
+        <p style=" margin-bottom: 8px;">A <span class="orange">CREATIVE</span> WEB DEVELOPER</p>
+       <div>
+      <a class="btn1" href="mailto:toluadekunte@gmail.com">CONTACT ME</a></div></div>
+   </div> 
+
+<div style=" margin-top: -100px;" class="polygon"> 
+<img src="https://res.cloudinary.com/de8awjxjn/image/upload/v1595353033/Polygon1.png" style="float: left; " class="d-inline-block align-top" alt=""><br/><br/>
+<img src="https://res.cloudinary.com/de8awjxjn/image/upload/v1595353033/Polygon2.png" style="float: left; " class="d-inline-block align-top" alt="">
+<img src="https://res.cloudinary.com/de8awjxjn/image/upload/v1595353033/Polygon2.png" style="float: right; " class="d-inline-block align-top" alt=""><br/><br/>
+<img src="https://res.cloudinary.com/de8awjxjn/image/upload/v1595353033/Polygon1.png" style="float: right; " class="d-inline-block align-top" alt="">
+
+
+</div>
+</center>
+</div>
+
+<div style="text-align:center; margin-top: 15%;" class="discover slideanim" >
+    <hr style="display:inline-block; position:relative; border: 1px solid #F8420B;  top:10px; width:20%" />
+       &nbsp;<span class="text-white" style="font-size: 18px;">DISCOVER MORE</span>&nbsp;
+    <hr style="display:inline-block; position:relative; border: 1px solid #F8420B; top:10px; width:22%" />
+</div>
+
+<div class="project slideanim" id="project">
+  <center><h1 class="project-head ">Works</h1></center>
+<div class="home-page-2 container slideanim">
+
+<div class="home-about ">
+<center>
+<div class="row">
+<div class="col ">
+   <img class="aboutpic" src="img/project3.png">
+</div>
+  <div class="col-sm " >
+  <div class="box-about">
+    <div class="line-about"><h1>STORE LOCATOR</h1>
+      <hr align="left" class="hr-works">
+    </div>
+    <br class="work-break">
+    <p>A store locator web application <a href="https://adtrex.github.io/storetracker/" class="orange">CLICK TO VISIT</a></p>
+  </div>
+</div>
+</div>
+  
+</div>
+  </center>
+</div>
+</div>
+
+<div class="home-page-2 container slideanim">
+
+<div class="home-about ">
+<center>
+<div class="row">
+<div class="col ">
+   <img class="aboutpic" src="img/project2.png">
+</div>
+  <div class="col-sm " >
+  <div class="box-about">
+    <div class="line-about"><h1>TECH UNKNOWN</h1>
+    <hr align="left" class="hr-works"></div>
+    <br class="work-break">
+    <p>A blog that keeps you updated on the latest technologies & gadgets <a href="https://www.techunknown.com" class="orange">CLICK TO VISIT</a></p>
+  </div>
+</div>
+</div>
+  
+</div>
+  </center>
+</div>
+</div>
+
+<div class="home-page-2 container slideanim">
+
+<div class="home-about ">
+<center>
+<div class="row">
+<div class="col ">
+   <img class="aboutpic" src="img/project1.png">
+</div>
+<br /><br />
+  <div class="col-sm " >
+  <div class="box-about">
+    <div class="line-about"><h1>ELDERLY LOVED ONES</h1>
+    <hr align="left" class="hr-works"></div>
+
+    <p>A consultancy website <a href="https://www.elderlylovedones.com" class="orange">CLICK TO VISIT</a></p>
+
+  </div>
+</div>
+</div>
+  
+</div>
+  </center>
+</div>
+</div>
+
+<div class="home-page-2 container slideanim">
+
+<div class="home-about ">
+<center>
+<div class="row">
+<div class="col ">
+   <img class="aboutpic" src="img/project4.png">
+</div>
+<br /><br />
+  <div class="col-sm " >
+  <div class="box-about">
+    <div class="line-about"><h1>GMG RADIO</h1>
+    <hr align="left" class="hr-works"></div>
+
+    <p>A website for a radio company <a href="https://www.gmgradioua.com" class="orange">CLICK TO VISIT</a></p>
+
+  </div>
+</div>
+</div>
+  
+</div>
+  </center>
+</div>
+</div>
+
+<div class="home-page-2 container slideanim">
+
+<div class="home-about ">
+<center>
+<div class="row">
+<div class="col ">
+   <img class="aboutpic" src="img/project5.png">
+</div>
+<br /><br />
+  <div class="col-sm " >
+  <div class="box-about">
+    <div class="line-about"><h1>ORANGE CARD</h1>
+    <hr align="left" class="hr-works"></div>
+
+    <p>A website for teachers to redeem a priviledge master card <a href="https://www.orangecard.ng" class="orange">CLICK TO VISIT</a></p>
+
+  </div>
+</div>
+</div>
+  
+</div>
+  </center>
+</div>
+</div>
+
+<div class="home-page-2 container slideanim">
+
+<div class="home-about ">
+<center>
+<div class="row">
+<div class="col ">
+   <img class="aboutpic" src="img/project6.png">
+</div>
+<br /><br />
+  <div class="col-sm " >
+  <div class="box-about">
+    <div class="line-about"><h1>KING ADEBUSS ENTERPRISES</h1>
+    <hr align="left" class="hr-works"></div>
+
+    <p>A website for an investment company <a href="https://kingadebussenterprise.com/" class="orange">CLICK TO VISIT</a></p>
+
+  </div>
+</div>
+</div>
+  
+</div>
+  </center>
+</div>
+</div>
+
+<div class="home-page-2 container slideanim">
+
+<div class="home-about ">
+<center>
+<div class="row">
+<div class="col ">
+   <img class="aboutpic" src="img/project7.png">
+</div>
+<br /><br />
+  <div class="col-sm " >
+  <div class="box-about">
+    <div class="line-about"><h1>GET VIRAL</h1>
+    <hr align="left" class="hr-works"></div>
+
+    <p>A website for advertisers & influencers <a href="https://getviral.ml" class="orange">CLICK TO VISIT</a></p>
+
+  </div>
+</div>
+</div>
+  
+</div>
+  </center>
+</div>
+</div>
+
+
+
+</div>
+
+<div class="about-me container" id="about">
+   <center><h1 class="about-head slideanim" >About Me</h1>
+
+   <p class="slideanim" style="text-align: justify;">My name is Adekunte Tolulope and I am the founder of <a href="https://www.techunknown.com" style="color:#F8420B; ">Techunknown</a> (a blog that keeps people updated on latest technology & gadgets) which has over 5000 audience. I have been in the programming world for over 3 years now and have attended various internships such as Hng Internship & start.ng. I have contributed to various projects and presently work with <a href="https://www.webcraddle.com" style="color:#F8420B;">WebCraddle</a> (a team of talented youth) to inspire people in this digital age.</p>
+  </center>
+</div>
+
+<div class="skills container" id="skills">
+<center><h1 class="about-head slideanim">Skills</h1>
+  <div class="container ">
+    <center>
+  <div class="row">
+    <div class="col-sm col-md-4 slideanim">
+      <i class="fas fa-laptop orange skills-icon"></i>
+      <h1>FrontEnd</h1>
+      <p>HTML(5)</p>
+      <p>CSS(3)</p>
+      <p>JavaScript</p>
+      <p>Jquery</p>
+      <p>Bootstrap</p>
+      <p>React</p>
+    </div>
+     <div class="col-sm col-md-4 slideanim">
+      <i class="fas fa-database orange skills-icon"></i>
+      <h1>BackEnd</h1>
+      <p>PHP</p>
+      <p>Laravel</p>
+      <p>SQL</p>
+      <p>C#</p>
+      <p>Restful Web Services</p>
+      <p>Python</p>
+      <p>Api</p>
+      <p>Devops</p>
+
+    </div>
+    <div class="col-sm col-md-4 slideanim">
+      <i class="fas fa-wrench orange skills-icon"></i>
+      <h1>Tools</h1>
+      <p>Github</p>
+      <p>Bitbucket</p>
+      <p>Heroku</p>
+      <p>Jest</p>
+      <p>AWS</p>
+
+    </div>
+  </div>
+    
+    
+
+
+</center>
+</div>
+
+<div class=" container" id="contact">
+  <center>
+    <div class="contact">
+  <h1 class="contact-head slideanim" >Want a website?</h1>
+  <hr class="contact-hr">
+  <div style="margin-top: 30px; margin-bottom: 30px;">
+  <p>I would build an amazing website for you</p>
+<a class="btn2" href="mailto:toluadekunte@gmail.com">CONTACT ME</a>
+</div>
+</div>
+
+  <h1 class="contact-head slideanim">Let's connect</h1>
+  <hr class="contact-hr">
+  <a class="orange font-icon" href="https://www.github.com/adtrex"><i class="fab fa-github"></i></a>
+  <a class="orange font-icon" href="https://www.instagram.com/adtrex_dev"><i class="fab fa-instagram"></i></a>
+  <a class="orange font-icon" href="https://www.twitter.com/adtrex_dev"><i class="fab fa-twitter"></i></a>
+  <a class="orange font-icon" href="https://www.linkedin.com/in/tolulope-adekunte-8a2891185/"><i class="fab fa-linkedin-in"></i></a>
+  
+</center>
+</div>
+
+
+<div>
+  <center>
+  <span class="icon-rotate">
+    <img src="https://res.cloudinary.com/de8awjxjn/image/upload/v1595353106/RECTANGLE.png" width="30" height="30" class="d-inline-block align-top" alt="">
+  </span>
+  <p id="copyright">Copyright<span class="orange"> &copy;</span> 2020 <span class="orange">|</span> Adekunte Tolulope</p>
+</center>
+</div>
+
+
+
+<script src="scripts/float-panel.js"></script>
+
+ <script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+</script>
+</body>
+
+
+</html>
